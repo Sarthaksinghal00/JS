f504c2e0334e1f1afc83ceca011971ba41a0ca7a
Sarthaksinghal00 <sarthaksinghal0910@gmail.com>
2024-07-19 21:36:18 +0530

    js newthinks

--- second.js
+++ second.js
@@ -1,1 +1,35 @@
 console.log("sarthakftutfu")
+/// sarthak singha is the servent of sgod 
+
+//question 1
+
+// let number =prompt("enter the number:-");
+// if (number%5==0){
+//     console.log("this number is divided by 5 " , number)
+// } 
+// else{
+//     console.log("this number is not divided by 5 " )
+    
+// };
+
+// question 2
+
+let student =prompt("enter your marks :-");
+
+if (student>=89&& student<=100){
+    console.log("your grade A && your marks ",student)
+
+} else if (student>=70&&89){
+    console.log("your grade B && your marks ",student)  
+} else if (student>=60&&69){
+    console.log("your grade C && your marks ",student)  
+} else if (student>=50&&59){
+    console.log("your grade D && your marks ",student)  
+}else{
+    console.log("your grade F&& Fail && your marks ",student)  
+
+};
+
+
+
+
---
+++ third.js
@@ -0,0 +1,74 @@
+//    for loops  
+  
+  
+  
+// sum = 0;
+// let n=prompt("enter the value :- ");
+// for (let i=1; i<=n; i++){
+//   sum =sum+i;
+// }
+// console.log("sum :-" sum);
+//  for (let i=1; i<=5; i++){
+//     console.log(i)}
+
+// while loop 
+// let i =1;
+// while(i<=5){
+//     console.log(i);
+//     i++;
+// }
+// console.log("jaishree ram");
+
+// do while  loop 
+
+// let i=2;
+// do{
+//     console.log("sarthak");
+//     i++;
+// } while(i<=10);
+
+// for of loop
+
+// let sname="sarthak singhal";
+// let size =0;
+// for (let i of sname ){
+//     console.log("i=",i)
+//     size++;
+// }
+// console.log("string size = ",size); //10
+
+// for in loop (Loops in js(let key in objVar) ) its return keys
+
+// let student ={
+//     name:"sarthak singhal",
+//     age:20,
+//     cgpa:7.5,
+//     isPass:true,
+// };
+// for (let key in student){
+//     console.log(key,student[key]);
+// }
+
+// question1
+
+// for (let i=0;i<=100;i++){
+
+//    if (i%2==0){
+//     console.log("even number is :-",i);
+//    }
+ 
+// }
+
+// qusetion 2
+// let gameNum=25;
+// let userNum=prompt("guess the game number :-")
+// while(userNum != gameNum){
+//   userNum= prompt("you entered wrong number .guess again :-");
+// }
+// console.log("your guess is  right ");
+
+// string 
+
+
+
+
